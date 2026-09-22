944696ce7eaf53ae295b52d0d9af269c5331cada
Serafim <sankevich.sima@gmail.com>
2026-09-22 20:42:44 +0300

    Add func HandlRoot and HandleUploat

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,29 @@
 package service
+
+import (
+	"fmt"
+	"pkg/morse"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func CheckMorse(text string) (string, error){
+	if strings.TrimSpace(text)==""{
+		return "", fmt.Errorf("empty string")
+	}
+
+	isNotMorse:=false
+	for _,char:=range text{
+		if char!=' '&&char!='.'&&char!='-'{
+			isNotMorse = true
+			break
+		}
+	}
+
+	if isNotMorse{
+		return morse.ToMorse(text), nil
+	} 
+	
+	return morse.ToText(text), nil
+}
